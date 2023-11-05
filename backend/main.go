a2f18665dca63a71d59d54c8e58e22a348f8fa3c
Nick Robins <nrobins11@live.com>
2023-11-05 16:24:46 -0600

    Initial commit

---
+++ backend/main.go
@@ -0,0 +1,151 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"github.com/plaid/plaid-go/v17/plaid"
+)
+
+func main() {
+    
+    r := gin.Default()
+    r.POST("/api/linktoken", createLinkToken)
+    r.POST("/api/publicToken", exchangePublicToken)
+    r.OPTIONS("/api/publicToken", allowCors)
+    r.GET("/api/transactions", getTransactions)
+
+    r.Run()
+}
+
+var (
+    client *plaid.APIClient
+    clientId string
+    secret string
+    accessToken string
+)
+
+func init() {
+    err := godotenv.Load()
+    if err != nil {
+        log.Fatal(err)
+    }
+    
+    clientId = os.Getenv("PLAID_CLIENT_ID")
+    secret = os.Getenv("PLAID_SECRET")
+
+    fmt.Printf("clientId: %s", clientId)
+    fmt.Printf("secret: %s", secret)
+    configuration := plaid.NewConfiguration()
+    configuration.AddDefaultHeader("PLAID-CLIENT-ID", clientId)
+    configuration.AddDefaultHeader("PLAID-SECRET", secret)
+    configuration.UseEnvironment(plaid.Sandbox)
+    client = plaid.NewAPIClient(configuration)
+}
+
+func allowCors(c *gin.Context) {
+    c.Header("Access-Control-Allow-Origin", "*")
+    c.Header("Access-Control-Allow-Headers", "Content-Type")
+    c.Header("Access-Control-Allow-Methods", "POST")
+}
+
+func createLinkToken(c *gin.Context) {
+    ctx := context.Background()
+    user := plaid.LinkTokenCreateRequestUser { ClientUserId: clientId }
+    request := plaid.NewLinkTokenCreateRequest(
+        "Plaid Test", "en", []plaid.CountryCode{plaid.COUNTRYCODE_US},
+        user,
+    )
+    request.SetProducts([]plaid.Products{plaid.PRODUCTS_AUTH})
+    request.SetLinkCustomizationName("default")
+    resp, httpResp, err := client.PlaidApi.LinkTokenCreate(ctx).LinkTokenCreateRequest(*request).Execute()
+    if err != nil {
+        fmt.Println(httpResp.Body)
+        log.Fatal(err)
+    }
+    linkToken := resp.GetLinkToken()
+    c.Header("Access-Control-Allow-Origin", "*")
+    c.JSON(http.StatusOK, gin.H{
+        "link_token": linkToken})
+}
+
+func exchangePublicToken(c *gin.Context) {
+    ctx := context.Background()
+    //publicToken := c.Request.Body.Read()("public_token")
+    d := json.NewDecoder(c.Request.Body)
+    d.DisallowUnknownFields()
+    t := struct {
+        Public_token string
+    }{}
+    err := d.Decode(&t) 
+    if err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{
+            "error": "public_token was empty",
+        })
+    }
+    fmt.Println(t)
+    publicToken := t.Public_token
+    fmt.Printf("publicToken: %s", publicToken)
+    exchangePublicTokenReq := plaid.NewItemPublicTokenExchangeRequest(publicToken)
+    exchangePublicTokenResp, httpResp , err := client.PlaidApi.ItemPublicTokenExchange(ctx).ItemPublicTokenExchangeRequest(
+        *exchangePublicTokenReq,
+    ).Execute()
+    if err != nil {
+        log.Fatal(httpResp.Body)
+    }
+    accessToken = exchangePublicTokenResp.GetAccessToken()
+    c.Header("Access-Control-Allow-Origin", "*")
+    c.JSON(http.StatusOK, gin.H{
+        "access_token": accessToken,
+    })
+}
+
+func getTransactions(c *gin.Context) {
+    accessToken = "access-sandbox-3bd62d49-418b-44b1-bdd9-95f887a0e519"
+    ctx := context.Background()
+    var cursor string
+    var added []plaid.Transaction
+    var modified []plaid.Transaction
+    var removed []plaid.RemovedTransaction
+    hasMore := true
+    //options := plaid.TransactionsSyncRequestOptions{
+    //    IncludePersonalFinanceCategory := true,
+    //}
+
+    for hasMore {
+        request := plaid.NewTransactionsSyncRequest(accessToken)
+        //request.SetOptions(options)
+        if cursor != "" {
+          request.SetCursor(cursor)
+        }
+        resp, httpResp, err := client.PlaidApi.TransactionsSync(
+            ctx,
+        ).TransactionsSyncRequest(*request).Execute()
+        if err != nil {
+            log.Fatal(httpResp.Body)
+        }
+
+        added = append(added, resp.GetAdded()...)
+        modified = append(modified, resp.GetModified()...)
+        removed = append(removed, resp.GetRemoved()...)
+
+        hasMore = resp.GetHasMore()
+
+        cursor = resp.GetNextCursor()
+    }
+    
+    c.Header("Access-Control-Allow-Origin", "*")
+    //enc := json.NewEncoder(c.Writer)
+    fmt.Println(added, modified, removed)
+    c.JSON(http.StatusOK, gin.H{
+        "added": added,
+    })
+}
+
+
