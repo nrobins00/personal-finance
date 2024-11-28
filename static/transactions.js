5b2ad5666689586d4c190ec6b082057fdacd32f0
Nick Robins <nrobins11@live.com>
2024-11-28 11:45:30 -0600

    better formatting for transactions

--- static/transactions.js
+++ static/transactions.js
@@ -1,34 +1,34 @@
 function filterCategories() {
-    var checkboxes = document.querySelectorAll('.dropdown-content input[type="checkbox"]');
-    const selectedCats = []
-    checkboxes.forEach((checkbox) => {
-        if (checkbox.checked) {
-            selectedCats.push(checkbox.value);
-        }
-    });
-
-    const transactions = document.querySelectorAll('[data-category]');
-    for (const element of transactions) {
-        if (!selectedCats.includes(element.dataset.category)) {
-            element.style.display = "none";
-        } else {
-            element.style.display = "block";
-        }
+  var checkboxes = document.querySelectorAll(
+    '.dropdown-content input[type="checkbox"]',
+  );
+  const selectedCats = [];
+  checkboxes.forEach((checkbox) => {
+    if (checkbox.checked) {
+      selectedCats.push(checkbox.value);
     }
+  });
+
+  const transactions = document.querySelectorAll("[data-category]");
+  for (const element of transactions) {
+    if (!selectedCats.includes(element.dataset.category)) {
+      element.style.display = "none";
+    } else {
+      element.style.display = "table-row";
+    }
+  }
 }
 
 function showDropdown() {
-    const dropdown = document.querySelector('.dropdown-content');
-    dropdown.style.display = "block";
+  const dropdown = document.querySelector(".dropdown-content");
+  dropdown.style.display = "block";
 }
 
-document.addEventListener('click', (event) => {
-    const dropdown = document.querySelector('.dropdown');
-    const dropdownContent = dropdown.querySelector('.dropdown-content');
-    if (!dropdown.contains(event.target)) {
-        dropdownContent.style.display = "none";
-        filterCategories();
-    }
-})
-
-
+document.addEventListener("click", (event) => {
+  const dropdown = document.querySelector(".dropdown");
+  const dropdownContent = dropdown.querySelector(".dropdown-content");
+  if (!dropdown.contains(event.target)) {
+    dropdownContent.style.display = "none";
+    filterCategories();
+  }
+});
